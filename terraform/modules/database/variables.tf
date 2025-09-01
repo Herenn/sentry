4288c89530d4955e0d938c41a19a20523326d42d
Eren Ciftci <erenciftci@hotmail.com.tr>
2025-09-02 00:05:13 +0300

    Add terraform/modules/database/variables.tf

---
+++ terraform/modules/database/variables.tf
@@ -0,0 +1,96 @@
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project name for resource naming"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC"
+  type        = string
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block of the VPC"
+  type        = string
+}
+
+variable "database_subnets" {
+  description = "List of database subnet IDs"
+  type        = list(string)
+}
+
+variable "engine" {
+  description = "Database engine"
+  type        = string
+  default     = "mysql"
+}
+
+variable "engine_version" {
+  description = "Database engine version"
+  type        = string
+  default     = "8.0"
+}
+
+variable "instance_class" {
+  description = "RDS instance class"
+  type        = string
+  default     = "db.t3.micro"
+}
+
+variable "allocated_storage" {
+  description = "Allocated storage in GB"
+  type        = number
+  default     = 20
+}
+
+variable "max_allocated_storage" {
+  description = "Maximum allocated storage in GB"
+  type        = number
+  default     = 100
+}
+
+variable "storage_type" {
+  description = "Storage type"
+  type        = string
+  default     = "gp2"
+}
+
+variable "database_name" {
+  description = "Name of the database"
+  type        = string
+  default     = "app"
+}
+
+variable "username" {
+  description = "Database master username"
+  type        = string
+  default     = "admin"
+}
+
+variable "password" {
+  description = "Database master password"
+  type        = string
+  sensitive   = true
+}
+
+variable "backup_retention_period" {
+  description = "Backup retention period in days"
+  type        = number
+  default     = 7
+}
+
+variable "backup_window" {
+  description = "Backup window"
+  type        = string
+  default     = "03:00-04:00"
+}
+
+variable "maintenance_window" {
+  description = "Maintenance window"
+  type        = string
+  default     = "sun:04:00-sun:05:00"
+}
