94ce95b1b01d23cf4e303e587b5345317c6bbdd2
Eren Ciftci <erenciftci@hotmail.com.tr>
2025-09-02 00:11:12 +0300

    Add terraform/envs/stage/outputs.tf

---
+++ terraform/envs/stage/outputs.tf
@@ -0,0 +1,35 @@
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "vpc_cidr_block" {
+  description = "CIDR block of the VPC"
+  value       = module.vpc.vpc_cidr_block
+}
+
+output "private_subnets" {
+  description = "List of IDs of private subnets"
+  value       = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  description = "List of IDs of public subnets"
+  value       = module.vpc.public_subnets
+}
+
+output "database_subnets" {
+  description = "List of IDs of database subnets"
+  value       = module.vpc.database_subnets
+}
+
+output "load_balancer_dns" {
+  description = "DNS name of the load balancer"
+  value       = module.compute.load_balancer_dns
+}
+
+output "database_endpoint" {
+  description = "RDS instance endpoint"
+  value       = module.database.endpoint
+  sensitive   = true
+}
